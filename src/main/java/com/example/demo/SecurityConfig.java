f52577b5d0c0ada1ceee7baed3b55286019ddb12
Mike Carey <mcarey@solstice.com>
2019-05-10 16:05:12 -0500

    Yes! Auth is disabled

---
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    // For example: Use only Http Basic and not form login.
+    http
+      .authorizeRequests()
+      .anyRequest().permitAll();
+  }
+}
